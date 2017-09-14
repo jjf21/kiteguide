46b1ed294e1e7eca02e642314fdeb9d603c6f5da
Carl Quivron <carl.quivron@gmail.com>
2017-09-14 22:02:25 +0200

    Added basics models for products

---
+++ db/migrate/20170914195824_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.references :brand, foreign_key: true
+      t.string :model
+      t.string :type
+      t.string :year
+
+      t.timestamps
+    end
+  end
+end
